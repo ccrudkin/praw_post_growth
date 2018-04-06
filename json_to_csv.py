5803c34e66e969e0570e9de395bb3010326c980d
ccrudkin <ccrudkin@gmail.com>
2018-04-06 09:40:42 -0600

    comments in json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,7 +1,12 @@
+# Not really necessary for data handling.
+
 import json
 import csv
+import os
 
-with open('C:/Users/ccrud/PycharmProjects/reddit_bot/praw_post_growth/Files/scores.json', 'r') as f:
+cwd = os.getcwd()
+
+with open(cwd + '/Files/scores.json', 'r') as f:
     data = json.load(f)
 
 csv_data = [['Submission ID', 'Age (minutes)', 'Karma']]
@@ -11,7 +16,7 @@ for id in data:
         if dp != '0':
             csv_data.append([id, data[id][dp][1], data[id][dp][0]])
 
-with open('C:/Users/ccrud/PycharmProjects/reddit_bot/karma_temp.csv', 'w', newline='') as c:
+with open(cwd + '/Files/karma_temp.csv', 'w', newline='') as c:
     outputwriter = csv.writer(c)
     for line in csv_data:
         outputwriter.writerow(line)
